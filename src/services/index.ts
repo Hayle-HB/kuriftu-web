562e537f69d733603bded2fc082fbbba00484850
brookdb <brksolomon@gmail.com>
2025-03-09 14:01:46 -0400

    hardcoded the test backend

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL || "https://your-api-base-url.com",
+  baseURL: process.env.REACT_APP_API_BASE_URL || "https://prod-backend.kurifturesorts.com",
   headers: {
     "Content-Type": "application/json",
   },
