5cce608c7d581864f1c7aad8d64966930ab68c15
brookdb <brksolomon@gmail.com>
2025-03-22 21:22:17 -0400

    Reduce the spacing between sections on the homepage.

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ function App() {
   return (
     <div className="app">
       <Header />
-      <Container className="App">
+      <Container className="App" fluid>
         <Outlet />
       </Container>
       <Footer />
