4126aaadb2147e89fb3d8779f23066834b4d780d
brookdb <brksolomon@gmail.com>
2025-03-24 22:53:03 -0400

    Capitalize 'Resort & Spa' for entoto data file

--- src/MockData/resortsDetails.tsx
+++ src/MockData/resortsDetails.tsx
@@ -3,7 +3,7 @@ import { ResortDetailsModal } from "../interfaces/resortDetailModal";
 
 export const RESORTDETAILS: Record<string, ResortDetailsModal> = {
   entoto: {
-    name: "Kuriftu resort & spa Entoto",
+    name: "Kuriftu Resort & Spa Entoto",
     region: "ENTOTO PARK, ADDIS ABABA",
     address: "321 Lake View Avenue, Bishoftu, Ethiopia",
     description:
